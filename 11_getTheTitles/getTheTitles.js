90cf6db1c6819893937cb7c48a39d5ff635716e2
Kevin Monteiro <kevin.monteiro96@gmail.com>
2022-05-30 23:14:56 +0100

    first pass exercise 11
    iterate through function parameter, add each title to array, and return array of titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,18 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // initiate array length and empty return array
+    booksLength = books.length;
+    console.log(booksLength); // check array length
+    let bookTitles = [];
 
+    /* Iterate through books list and push each
+    elements title to empty array */
+    for (let i = 0; i < booksLength; i++) {
+        console.log(books[i].title); // check book titles being iterated through
+        bookTitles.push(books[i].title);
+    };
+    
+    // return array of titles
+    return bookTitles;
 };
 
 // Do not edit below this line
