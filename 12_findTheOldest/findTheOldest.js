fc868eec49d57c210e07954f0cd7e3021f623c7c
Kevin Monteiro <kevin.monteiro96@gmail.com>
2022-05-30 23:30:41 +0100

    first pass pseudocode exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+/* given array of people with name, DOB and
+date of death (DOD) findTheOldest person*/
+
+/* iterate through people array and calc
+difference between DOB and DOD */
+
+// if not dead, set year of death to this year
+
+// find largest age
+
+// find index of age
+
+// return persons name using index
 
 };
 
