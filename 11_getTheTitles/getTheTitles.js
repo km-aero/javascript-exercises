81f02597bdec4c37a92b80a69b45802fc1699a7d
Kevin Monteiro <kevin.monteiro96@gmail.com>
2022-05-30 23:16:01 +0100

    chore: remove test console logging

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,13 +1,11 @@
 const getTheTitles = function(books) {
     // initiate array length and empty return array
     booksLength = books.length;
-    console.log(booksLength); // check array length
     let bookTitles = [];
 
     /* Iterate through books list and push each
     elements title to empty array */
     for (let i = 0; i < booksLength; i++) {
-        console.log(books[i].title); // check book titles being iterated through
         bookTitles.push(books[i].title);
     };
     
