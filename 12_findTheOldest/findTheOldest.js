2d0ed5a8103a5d051eeda9a04d4289e3b43863fb
Kevin Monteiro <kevin.monteiro96@gmail.com>
2022-05-30 23:55:26 +0100

    first draft failing tests due to not finding name key

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,17 +2,28 @@ const findTheOldest = function(people) {
 /* given array of people with name, DOB and
 date of death (DOD) findTheOldest person*/
 
+// initialise variables
+const peopleLength = people.length;
+let age = [];
+
 /* iterate through people array and calc
 difference between DOB and DOD */
-
-// if not dead, set year of death to this year
+for (let i = 0; i < peopleLength; i++) {
+    // if not dead, set year of death to this year
+    if (typeof people[i].yearOfDeath === undefined) {
+        people[i].yearOfDeath = new Date().getFullYear();
+    };
+    age.push(people[i].yearOfDeath - people[i].yearOfBirth);
+};
 
 // find largest age
+const oldestAge = Math.max(age);
 
 // find index of age
+const oldestAgeIndex = age.indexOf(oldestAge);
 
 // return persons name using index
-
+return people[oldestAgeIndex].name;
 };
 
 // Do not edit below this line
